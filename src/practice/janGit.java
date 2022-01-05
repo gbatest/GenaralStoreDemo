241d26f26c9f7b5e899834b4332f1ae0f9c49f72
ganga <gbatest2021@gmail.com>
2022-01-05 17:16:22 -0600

    GST_gitJan5

---
+++ src/practice/janGit.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class janGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("git demo project2_test1");
+	}
+
+}
